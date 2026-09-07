cccdd320a675f1fd4f6aec27afa6f8c62a48898d
lexxkrt <lexxkrt@gmail.com>
2026-09-07 23:54:27 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,3 @@
 <?php
 
-use App\Services\ImageService;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 require __DIR__.'/admin.php';
-
-Route::get('/image/{size}/{image}', function ($size, $image) {
-    return (new ImageService)->getImage($image, $size);
-});
